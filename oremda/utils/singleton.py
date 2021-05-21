64ca5c9fce170c299894166d266a6a4f06e3e1bc
Patrick Avery <patrick.avery@kitware.com>
2021-05-21 15:40:06 -0500

    Add singleton helper class
    If a class inherits from Singleton, it becomes a singleton...
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

---
+++ oremda/utils/singleton.py
@@ -0,0 +1,15 @@
+class SingletonMetaclass(type):
+    """A metaclass for singletons"""
+
+    _instance = None
+
+    def __call__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__call__(*args, **kwargs)
+
+        return cls._instance
+
+
+class Singleton(metaclass=SingletonMetaclass):
+    """Inherit from this class to easily use the metaclass singleton..."""
+    pass
