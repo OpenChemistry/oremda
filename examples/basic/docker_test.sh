6d2c576327e5a9916705332a02e7edebc643dce9
Patrick Avery <patrick.avery@kitware.com>
2021-06-04 10:21:33 -0500

    Perform better cleanup in docker test
    This removes the containers properly. Previously, the plasma store
    would never even be stopped.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- examples/basic/docker_test.sh
+++ examples/basic/docker_test.sh
@@ -3,10 +3,10 @@
 OREMDA_VAR_DIR=/run/oremda
 prefix=basic_example_
 
-plasma_id=$(docker run -d --ipc="shareable" -v $OREMDA_VAR_DIR:$OREMDA_VAR_DIR   oremda/${prefix}plasma)
+plasma_id=$(docker run --rm -d --ipc="shareable" -v $OREMDA_VAR_DIR:$OREMDA_VAR_DIR   oremda/${prefix}plasma)
 loader_id=$(docker run -d --ipc="container:$plasma_id" -v $OREMDA_VAR_DIR:$OREMDA_VAR_DIR     oremda/${prefix}loader)
-add_id=$(docker run -d --ipc="container:$plasma_id" -v $OREMDA_VAR_DIR:$OREMDA_VAR_DIR oremda/${prefix}add)
-multiply_id=$(docker run -d --ipc="container:$plasma_id" -v $OREMDA_VAR_DIR:$OREMDA_VAR_DIR oremda/${prefix}multiply)
+add_id=$(docker run --rm -d --ipc="container:$plasma_id" -v $OREMDA_VAR_DIR:$OREMDA_VAR_DIR oremda/${prefix}add)
+multiply_id=$(docker run --rm -d --ipc="container:$plasma_id" -v $OREMDA_VAR_DIR:$OREMDA_VAR_DIR oremda/${prefix}multiply)
 view_id=$(docker run -d --ipc="container:$plasma_id" -v $OREMDA_VAR_DIR:$OREMDA_VAR_DIR oremda/${prefix}view)
 
 # Wait for the loader to finish
@@ -14,3 +14,8 @@ docker wait $loader_id
 
 docker logs $loader_id
 docker logs $view_id
+
+docker container rm $loader_id $view_id
+
+# Plasma does not stop automatically... we should fix that
+docker kill $plasma_id
