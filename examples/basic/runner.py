47d186e274f80d9fcf110e00313a5dd0855f7e60
Patrick Avery <patrick.avery@kitware.com>
2021-06-04 14:06:15 -0500

    Run singularity client without instances
    This updates the singularity client to run without instances, as
    they are more complicated and will not be supported in other container
    technologies.
    
    This means, however, that we will have a harder time killing singularity
    processes, as the singularity python API does not provide any way to
    kill the process, nor does it provide things like the subprocess id.
    
    However, the containers have been updated so that they close cleanly now.
    So we do not need to worry as much about that for singularity.
    
    This also updates runner.py so that it will work with singularity also.
    It seems to be working.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- examples/basic/runner.py
+++ examples/basic/runner.py
@@ -1,104 +1,79 @@
 #!/usr/bin/env python
 
-from contextlib import ExitStack, contextmanager
+from pathlib import Path
 import time
 
-import docker
+from oremda.clients import Client
+from oremda.clients.utils import run_container, run_containers
 
 
-@contextmanager
-def run_container(name, *args, **kwargs):
-    client = docker.from_env()
-    container = client.containers.run(name, *args, **kwargs)
-    try:
-        yield container
-    except Exception as e:
-        print(f'An exception was caught: {e}')
-        print('Logs:', container.logs().decode('utf-8'))
-        raise
-    finally:
-        container.stop()
-        container.remove(v=True)
-
-
-@contextmanager
-def run_containers(names, args_list, kwargs_list):
-    with ExitStack() as stack:
-        containers = []
-        try:
-            for name, args, kwargs in zip(names, args_list, kwargs_list):
-                c = stack.enter_context(run_container(name, *args, **kwargs))
-                containers.append(c)
-
-            yield containers
-        except Exception as e:
-            print(f'An exception was caught: {e}')
-            print('Logs:')
-            for container in containers:
-                print(f'{container.id}: {container.logs().decode("utf-8")}')
-            raise
+SINGULARITY_DIR = Path.cwd() / '.singularity'
 
 
 if __name__ == '__main__':
-    client = docker.from_env()
+
+    client = Client('docker')
 
     # Docker name prefixes
     prefix = 'basic_example_'
 
-    run_list = [
-        f'oremda/{prefix}times_two',
-        f'oremda/{prefix}minus_three',
-        f'oremda/{prefix}viewer',
+    operator_list = [
+        'add',
+        'multiply',
+        'view',
     ]
+    if client.type == 'docker':
+        run_list = [f'oremda/{prefix}{name}' for name in operator_list]
+        loader_image = f'oremda/{prefix}loader'
+    elif client.type == 'singularity':
+        suffix = '.simg'
+        run_list = [f'{SINGULARITY_DIR}/{prefix}{name}{suffix}'
+                    for name in operator_list]
+        loader_image = f'{SINGULARITY_DIR}/{prefix}loader{suffix}'
+    else:
+        raise Exception(f'Unknown client type: {client.type}')
 
-    # Queue names must begin with "/"
-    base_queue_name = '/queue'
-    queue_names = [f'{base_queue_name}{i}' for i in range(len(run_list))]
+    volumes = {
+        '/run/oremda': {
+            'bind': '/run/oremda',
+        }
+    }
 
-    loader_image = f'oremda/{prefix}loader'
     loader_kwargs = {
-        # The loader will create all of the queues
-        'command': queue_names,
         'ipc_mode': 'shareable',
         'detach': True,
+        'volumes': volumes,
     }
 
     # Keep the loader running until the end, as it hosts the IPC shared memory
-    with run_container(loader_image, **loader_kwargs) as load_container:
-        # Wait for the load container to start
-        while load_container.status != 'created':
-            print(f'{load_container.status=}')
-            time.sleep(1)
+    with run_container(client, loader_image,
+                       **loader_kwargs) as loader_container:
+        # Wait for the loader container to start
+        # It'd be nice if we had a cleaner way to do this for singularity...
+        time.sleep(10)
 
         default_kwargs = {
-            'ipc_mode': f'container:{load_container.id}',
+            'ipc_mode': f'container:{loader_container.id}',
             'detach': True,
+            'volumes': volumes,
         }
-        containers = [load_container]
+        containers = [loader_container]
         args_list = []
         kwargs_list = []
         for i, name in enumerate(run_list):
-            kwargs = default_kwargs.copy()
-            from_queue_name = f'{base_queue_name}{i}'
-            if i < len(run_list) - 1:
-                to_queue_name = f'{base_queue_name}{i + 1}'
-                kwargs['command'] = [from_queue_name, to_queue_name]
-            else:
-                kwargs['command'] = [from_queue_name]
-
             args_list.append([])
-            kwargs_list.append(kwargs)
+            kwargs_list.append(default_kwargs)
 
         # Run the containers
-        with run_containers(run_list, args_list,
+        with run_containers(client, run_list, args_list,
                             kwargs_list) as run_containers:
             containers += run_containers
-            # The final container tells the load container to finish.
-            # Wait for the load container to finish.
-            load_container.wait()
+            # Wait for the loader container to finish.
+            loader_container = run_containers[0]
+            loader_container.wait()
 
             # Print out the logs
             for container in containers:
                 output = container.logs()
                 if output:
-                    print(output.decode('utf-8'))
+                    print(output)
--- oremda/clients/singularity.py
+++ oremda/clients/singularity.py
@@ -21,11 +21,8 @@ class SingularityClient(ClientBase):
         kwargs = self._docker_kwargs_to_singularity(kwargs)
 
         name = Path(image).stem
-        options = kwargs.get('options')
-        container = SingularityContainer(image, name, options=options)
-
-        if args:
-            container.run(*args, **kwargs)
+        container = SingularityContainer(image, name)
+        container.run(*args, **kwargs)
 
         return container
 
@@ -52,42 +49,49 @@ class SingularityClient(ClientBase):
 
 class SingularityContainer(ContainerBase):
 
-    def __init__(self, image, name=None, options=None):
-        self.instance = client.instance(image, name=name, options=options)
+    def __init__(self, image, name=None):
+        self.image = image
+        self.name = name
+
         self._logs_lock = Lock()
         self._logs = []
         self._stream_command = None
+        self.future = None
 
     @property
     def id(self):
-        return self.instance.name
+        return self.name
 
     def __del__(self):
         self.stop()
 
     def run(self, *args, **kwargs):
         if self._stream_command is not None:
-            msg = f'A command is already in instance {self.instance.name}'
+            msg = f'A command is already running for {self.id}'
             raise Exception(msg)
 
         # Clear the logs
         with self._logs_lock:
             self._logs.clear()
 
-        # Always stream
+        # Run in a background thread
+        self.future = ThreadPoolSingleton().submit(self._run, *args, **kwargs)
+
+    def _run(self, *args, **kwargs):
+        # This function should run in a background thread
+
         kwargs = kwargs.copy()
+        kwargs['image'] = self.image
+
+        # Always stream
         kwargs['stream'] = True
 
-        self._stream_command = client.run(self.instance, *args, **kwargs)
+        # spython will error if we don't return the result...
+        kwargs['return_result'] = True
 
-        # Append to the logs in a background thread
-        ThreadPoolSingleton().submit(self._read_logs)
+        self._stream_command = client.run(**kwargs)
 
-    def _read_logs(self):
-        # This function should run in a background thread
-        if not self._stream_command:
-            return
-
+        # Write logs as they come in
         for output in self._stream_command:
             with self._logs_lock:
                 self._logs.append(output)
@@ -99,12 +103,22 @@ class SingularityContainer(ContainerBase):
             return ''.join(self._logs)
 
     def stop(self, *args, **kwargs):
-        instance_names = [x.name for x in client.instances()]
-        if self.instance.name not in instance_names:
-            # It isn't running
-            return
+        # We cannot force kill the singularity containers right now,
+        # mostly because the singularity api doesn't give us a way to
+        # kill the singularity subprocess. We may need to call subprocess
+        # ourselves if we need to force kill the singularity subprocess in
+        # the future.
 
-        self.instance.stop()
+        # Instead, wait for the future to finish
+        self.wait()
 
     def remove(self, *args, **kwargs):
         self.stop()
+
+    def wait(self, *args, **kwargs):
+        if self.future is None:
+            # No process to wait on
+            return
+
+        self.future.result()
+        self.future = None
