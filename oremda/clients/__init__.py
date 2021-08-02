c188805c7a17967d780c7d12ac7e05bd7fab0003
Patrick Avery <patrick.avery@kitware.com>
2021-08-02 15:10:13 -0500

    Add function to auto determine client from image
    This currently assumes that if there is a colon in the image or
    if the file path does not exist, that it is Docker. Otherwise, it
    assumes it is Singularity.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- oremda/clients/__init__.py
+++ oremda/clients/__init__.py
@@ -1,3 +1,4 @@
+from pathlib import Path, PurePath
 from typing import Union
 
 from oremda.clients.base.client import ClientBase
@@ -18,3 +19,15 @@ def Client(type: Union[ContainerType, str] = ContainerType.Docker) -> ClientBase
         raise Exception(f'Unknown container type: {type}')
 
     return container_types[type]()
+
+
+def client_from_image(image_path: Union[str, PurePath]) -> ClientBase:
+    # Try to automatically determine the type of client based upon
+    # the image provided, and return it.
+
+    if ':' in image_path or not Path(image_path).exists():
+        # If it contains a colon, or the file path does not exist,
+        # assume it is docker.
+        return Client('docker')
+
+    return Client('singularity')
