e31a3a13840b5305766d4fa44e3d32c95365f601
Patrick Avery <patrick.avery@kitware.com>
2021-09-03 14:12:15 -0500

    Quick fix: fix multi-threading MPI issue on nersc
    FIXME: multi-threading with send() and recv() should work. Figure
    out why it is not working, and then remove this commit.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- oremda/messengers/mpi/messenger.py
+++ oremda/messengers/mpi/messenger.py
@@ -1,5 +1,7 @@
 from mpi4py import MPI
 from pydantic import validate_arguments
+from threading import Lock
+import time
 
 from oremda.constants import DEFAULT_PLASMA_SOCKET_PATH
 from oremda.messengers.base import BaseMessenger
@@ -8,6 +10,7 @@ from oremda.typing import Message, Port
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
+comm_lock = Lock()
 
 
 class MPIMessenger(BaseMessenger):
@@ -28,11 +31,38 @@ class MPIMessenger(BaseMessenger):
     @validate_arguments
     def send(self, msg: Message, dest: int):
         msg = self.detach_data(dict(msg))
-        comm.send(msg, dest=dest)
+        with comm_lock:
+            req = comm.isend(msg, dest=dest)
+
+        while True:
+            time.sleep(1)
+            with comm_lock:
+                output = req.test()
+                if output[0]:
+                    return
 
     @validate_arguments
     def recv(self, source: int) -> Message:
-        msg = comm.recv(source=source)
+        with comm_lock:
+            # recv can probe the needed buffer size beforehand and allocate
+            # it automatically. But irecv needs a big enough buffer size
+            # without probing, unfortunately. So we have to allocate a big
+            # enough buffer every time. Let's do 1 MB for now.
+            req = comm.irecv(bytearray(1 << 20), source=source)
+
+        while True:
+            time.sleep(1)
+            with comm_lock:
+                try:
+                    output = req.test()
+                except Exception as e:
+                    print(f"Caught exception on {rank=} from {source=}", e)
+                    print(f"{comm.probe(source=source)=}")
+                    raise
+                if output[0]:
+                    msg = output[1]
+                    break
+
         msg = self.join_data(msg)
         return Message(**msg)
 
