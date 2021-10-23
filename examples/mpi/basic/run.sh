475c1f48095f8d9e44e588d5484d8de69619a2ea
Patrick Avery <patrick.avery@kitware.com>
2021-10-22 19:44:59 -0500

    Update basic MPI example to use "oremda run"
    This seems to work in my testing.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- examples/mpi/basic/run.sh
+++ examples/mpi/basic/run.sh
@@ -2,12 +2,20 @@
 #SBATCH --account=ncemhub
 #SBATCH --qos=debug
 #SBATCH -C haswell
-#SBATCH --time=00:01:00
+#SBATCH --time=00:10:00
 #SBATCH -N 8
 
 home=/global/homes/p/psavery/
 venv_path=$home/virtualenvs
 oremda_path=$venv_path/oremda
 
+export MPICH_MAX_THREAD_SAFETY=multiple
+export PYTHONUNBUFFERED=1
+
+export OREMDA_DATA_DIR=$SCRATCH/data/oremda
+export OREMDA_CONTAINER_TYPE=singularity
+export OREMDA_SIF_DIR=images
+export OREMDA_OPERATOR_CONFIG_FILE="operator_config.json"
+
 source $oremda_path/bin/activate
-srun -n 8 python -u run.py
+srun -n 8 oremda run pipeline.json
