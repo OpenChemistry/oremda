cefb4e9673e2b3a9ea45a6fb3332cad94d0a5a35
Chris Harris <chris.harris@kitware.com>
2022-01-26 11:32:41 -0500

    Use resourcs.path(...) as its available in Python 3.8

--- client/oremda/client/__init__.py
+++ client/oremda/client/__init__.py
@@ -2,7 +2,8 @@ from importlib import resources
 from pathlib import Path
 from fastapi.staticfiles import StaticFiles
 
-client_package_path = resources.files("oremda.client")
+with resources.path("oremda.client", "__init__.py") as path:
+    client_package_path = path.parent
 
 client_build = StaticFiles(
     directory=Path(str(client_package_path)) / "build", html=True
