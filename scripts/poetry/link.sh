2f72e1a472bbff254b7c71a77e31fdfc460c096c
Chris Harris <chris.harris@kitware.com>
2022-02-10 15:20:47 -0500

    Clear up debug

--- scripts/poetry/link.sh
+++ scripts/poetry/link.sh
@@ -7,9 +7,7 @@ cd $script_dir/../../
 
 for package_dir in $package_dirs
 do
-    package_dir=$(dirname "$project_file")
-    name=$(echo $package_dir | sed 's/\.\///g')
-    echo "Linking oremda-$name"
+    echo "Linking oremda-$package_dir"
     cd $package_dir
     oremda_packages=$(poetry show | grep "^oremda-" | sed "s/oremda-\([a-z]*\).*/\1/g")
     package_paths=""
