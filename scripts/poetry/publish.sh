2893e2f69289760303c45da76e236966d12ca5e9
Chris Harris <chris.harris@kitware.com>
2022-02-11 15:56:35 -0500

    Reset versions and switch to main PyPi index

--- scripts/poetry/publish.sh
+++ scripts/poetry/publish.sh
@@ -10,7 +10,7 @@ else
   package_name="oremda-${package_name}"
 fi
 
-current_published_version=$(curl https://test.pypi.org/pypi/${package_name}/json 2>/dev/null | jq '.info.version')
+current_published_version=$(curl https://pypi.org/pypi/${package_name}/json 2>/dev/null | jq '.info.version')
 if [ $? -ne 0 ]
 then
   echo "Package version not found for: $package_name"
@@ -24,5 +24,5 @@ if [ "$current_published_version" = "$current_version" ]; then
     echo "Version has not changed, nothing to publish."
 else
     rm -rf dist/
-    poetry publish -n --build -u $PYPI_USER -r $PYPI_REPO
+    poetry publish -n --build -u __token__
 fi
