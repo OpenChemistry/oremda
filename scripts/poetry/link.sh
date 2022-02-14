6818d8f078288b4f9cb5a3eb9eb1538ef6079056
Chris Harris <chris.harris@kitware.com>
2022-02-14 15:19:44 -0500

    Ensure packages are linked with develop=true

--- scripts/poetry/link.sh
+++ scripts/poetry/link.sh
@@ -21,3 +21,19 @@ do
     fi
     cd ..
 done
+
+# finally until poetry 1.2 is released we need to manually add the develop=true
+# option.
+pyproject_files=`find . -name pyproject.toml | xargs -n1 grep -l "^oremda-.*"`
+for pyproject_file in $pyproject_files
+do
+    sed -i 's/{path = \([^,]*\)}$/{path = \1, develop = true}/g' $pyproject_file
+done
+
+# We need to call update, for the develop=true to take effect
+for package_dir in $package_dirs
+do
+    cd $package_dir
+    poetry update
+    cd ..
+done
