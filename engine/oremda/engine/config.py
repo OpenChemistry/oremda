789ab7339c881545991c3a7e7a161ebe6dc6739d
Chris Harris <chris.harris@kitware.com>
2022-02-14 10:24:43 -0500

    Provide settings defaults

--- engine/oremda/engine/config.py
+++ engine/oremda/engine/config.py
@@ -1,12 +1,15 @@
+import tempfile
+from typing import Optional
 from oremda.typing import ContainerType
 from pydantic import BaseSettings
+from pathlib import Path
 
 
 class Settings(BaseSettings):
     SERVER_URL: str = "ws://localhost:8000/api/v1/rpc"
-    OREMDA_VAR_DIR: str
-    OREMDA_DATA_DIR: str
-    OREMDA_DIR: str
+    OREMDA_VAR_DIR: str = tempfile.gettempdir()
+    OREMDA_DATA_DIR: str = str(Path.home())
+    OREMDA_DIR: Optional[str]
     OREMDA_CONTAINER_TYPE: ContainerType = ContainerType.Docker
     OREMDA_SINGULARITY_IMAGE_DIR: str = ""
 
