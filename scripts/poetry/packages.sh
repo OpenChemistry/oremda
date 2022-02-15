2e3930006020ceada672f9d4103c4e99a6d7fee0
Chris Harris <chris.harris@kitware.com>
2022-02-15 11:16:29 -0500

    Fix shebang

--- scripts/poetry/packages.sh
+++ scripts/poetry/packages.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
+
 # Topologically sort list of package directories
 package_dirs="core cli engine server api meta"
--- scripts/poetry/publish_all.sh
+++ scripts/poetry/publish_all.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source "${script_dir}/packages.sh"
